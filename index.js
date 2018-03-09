c24bfcef5ee8a6674213ea76b720c50bb4f47115
Alvan <alvan@wemolive.cn>
2018-03-09 09:24:23 +0800

    upgrade to v2

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 'use strict';
 
-module.exports = require('./lib');
+const Mobydick = require('./lib/core');
+
+exports = module.exports = new Mobydick();
+
+exports.paging = require('./lib/paging');
+exports.utils = require('./lib/utils');
+
+exports.mvc = require('./lib-legacy/mvc');
+exports.route = require('./lib-legacy/route');
+exports.logger = require('./lib-legacy/logger');
+exports.page = require('./lib-legacy/page');
+exports.validator = require('./lib-legacy/validator');
